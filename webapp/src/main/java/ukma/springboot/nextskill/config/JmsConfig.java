df6d802335c65ee30fef157520b39afd51ab443f
Vladyslav Hibskyi <vgibskiy@gmail.com>
2025-11-06 19:49:40 +0200

    some changes

--- webapp/src/main/java/ukma/springboot/nextskill/config/JmsConfig.java
+++ webapp/src/main/java/ukma/springboot/nextskill/config/JmsConfig.java
@@ -3,9 +3,11 @@ package ukma.springboot.nextskill.config;
 import jakarta.jms.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
 
 @Configuration
+@EnableJms
 public class JmsConfig {
 
     @Bean
