de7ea6fe3e451d91ddd78290e6bb366161eb931a
Rinat Rafikov <rafikov.rinat.m@gmail.com>
2025-11-11 22:35:36 +0200

    add security configuration for admin server and update application properties

--- admin-server/build.gradle.kts
+++ admin-server/build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 dependencies {
     implementation("de.codecentric:spring-boot-admin-starter-server")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 }
-
